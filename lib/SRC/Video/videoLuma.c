1063544a0edb5094d1d8265828f00afd2337ffa7
Peter Sutton <foxxy@foxdogstudios.com>
2017-08-19 10:31:15 +0100

    Fix whitespace

--- lib/SRC/Video/videoLuma.c
+++ lib/SRC/Video/videoLuma.c
@@ -92,7 +92,7 @@ static void arVideoLumaARGBtoL_Intel_simd_asm(uint8_t * __restrict dest, uint8_t
 ARVideoLumaInfo *arVideoLumaInit(int xsize, int ysize, AR_PIXEL_FORMAT pixFormat)
 {
     ARVideoLumaInfo *vli;
-    
+
     vli = (ARVideoLumaInfo *)calloc(1, sizeof(ARVideoLumaInfo));
     if (!vli) {
         ARLOGe("Out of memory!!\n");
@@ -101,14 +101,14 @@ ARVideoLumaInfo *arVideoLumaInit(int xsize, int ysize, AR_PIXEL_FORMAT pixFormat
     vli->xsize = xsize;
     vli->ysize = ysize;
     vli->buffSize = xsize*ysize;
-	vli->buff = (ARUint8 *)AR_PAGE_ALIGNED_ALLOC(vli->buffSize);
+        vli->buff = (ARUint8 *)AR_PAGE_ALIGNED_ALLOC(vli->buffSize);
     if (!vli->buff) {
         ARLOGe("Out of memory!!\n");
         free(vli);
         return (NULL);
     }
     vli->pixFormat = pixFormat;
-    
+
     // Accelerated RGB to luma conversion.
 #if defined(HAVE_ARM_NEON) || defined(HAVE_ARM64_NEON) || defined(HAVE_INTEL_SIMD)
     vli->fastPath = (xsize * ysize % 8 == 0
@@ -131,10 +131,10 @@ ARVideoLumaInfo *arVideoLumaInit(int xsize, int ysize, AR_PIXEL_FORMAT pixFormat
     // Debug output.
 #  if defined(HAVE_ARM_NEON) || defined(HAVE_ARM64_NEON)
     if (vli->fastPath) ARLOGi("arVideoLuma will use ARM NEON acceleration.\n");
-	else ARLOGd("arVideoLuma will NOT use ARM NEON acceleration.\n");
+        else ARLOGd("arVideoLuma will NOT use ARM NEON acceleration.\n");
 #  elif defined(HAVE_INTEL_SIMD)
     if (vli->fastPath) ARLOGi("arVideoLuma will use Intel SIMD acceleration.\n");
-	else ARLOGd("arVideoLuma will NOT use Intel SIMD acceleration.\n");
+        else ARLOGd("arVideoLuma will NOT use Intel SIMD acceleration.\n");
 #  endif
 #endif
 
@@ -145,11 +145,11 @@ int arVideoLumaFinal(ARVideoLumaInfo **vli_p)
 {
     if (!vli_p) return (-1);
     if (!*vli_p) return (0);
-    
-	AR_PAGE_ALIGNED_FREE((*vli_p)->buff);
+
+        AR_PAGE_ALIGNED_FREE((*vli_p)->buff);
     free(*vli_p);
     *vli_p = NULL;
-    
+
     return (0);
 }
 
@@ -262,17 +262,17 @@ static void arVideoLumaBGRAtoL_ARM_neon(uint8_t * __restrict dest, uint8_t * __r
     uint8x8_t gfac = vdup_n_u8 (G8_CCIR601);
     uint8x8_t bfac = vdup_n_u8 (B8_CCIR601);
     int n = numPixels / 8;
-    
+
     // Convert per eight pixels.
     for (i = 0; i < n; i++) {
         uint16x8_t  temp;
         uint8x8x4_t rgb  = vld4_u8 (src);
         uint8x8_t result;
-        
+
         temp = vmull_u8 (rgb.val[0],      bfac);
         temp = vmlal_u8 (temp,rgb.val[1], gfac);
         temp = vmlal_u8 (temp,rgb.val[2], rfac);
-        
+
         result = vshrn_n_u16 (temp, 8);
         vst1_u8 (dest, result);
         src  += 8*4;
@@ -293,7 +293,7 @@ static void arVideoLumaBGRAtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "vdup.8      d4, r4           \n"
                      "vdup.8      d5, r5           \n"
                      "vdup.8      d6, r6           \n"
-                     "0:						   \n"
+                     "0:                                                   \n"
                      "# load 8 pixels:             \n"
                      "vld4.8      {d0-d3}, [%1]!   \n" // B into d0, G into d1, R into d2, A into d3.
                      "# do the weight average:     \n"
@@ -321,7 +321,7 @@ static void arVideoLumaRGBAtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "vdup.8      d4, r4           \n"
                      "vdup.8      d5, r5           \n"
                      "vdup.8      d6, r6           \n"
-                     "0:						   \n"
+                     "0:                                                   \n"
                      "# load 8 pixels:             \n"
                      "vld4.8      {d0-d3}, [%1]!   \n" // R into d0, G into d1, B into d2, A into d3.
                      "# do the weight average:     \n"
@@ -349,7 +349,7 @@ static void arVideoLumaABGRtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "vdup.8      d4, r4           \n"
                      "vdup.8      d5, r5           \n"
                      "vdup.8      d6, r6           \n"
-                     "0:						   \n"
+                     "0:                                                   \n"
                      "# load 8 pixels:             \n"
                      "vld4.8      {d0-d3}, [%1]!   \n" // A into d0, B into d1, G into d2, R into d3.
                      "# do the weight average:     \n"
@@ -377,7 +377,7 @@ static void arVideoLumaARGBtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "vdup.8      d4, r4           \n"
                      "vdup.8      d5, r5           \n"
                      "vdup.8      d6, r6           \n"
-                     "0:						   \n"
+                     "0:                                                   \n"
                      "# load 8 pixels:             \n"
                      "vld4.8      {d0-d3}, [%1]!   \n" // A into d0, R into d1, G into d2, B into d3.
                      "# do the weight average:     \n"
@@ -404,7 +404,7 @@ static void arVideoLumaBGRAtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "    movi        v0.8b,  #29            \n" // Load v0 with 8 copies of the 8 LSBs of L B factor =  0.114. 29/256 =  0.113 (29 = 0x001d).
                      "    movi        v1.8b,  #150           \n" // Load v1 with 8 copies of the 8 LSBs of L G factor =  0.587. 150/256 =  0.586 (150 = 0x0096).
                      "    movi        v2.8b,  #77            \n" // Load v2 with 8 copies of the 8 LSBs of L R factor =  0.299. 77/256 =  0.301 (77 = 0x004d).
-                     "0:						             \n"
+                     "0:                                                             \n"
                      // Read 8 BGRA pixels.
                      "    ld4         {v3.8b - v6.8b}, [%1],#32 \n" // Load 8 BGRA pixels, de-interleaving B into lower half of v3, G into lower half of v4, R into lower half of v5, A into lower half of v6.
                      // Do the weight average.
@@ -429,7 +429,7 @@ static void arVideoLumaRGBAtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "    movi        v0.8b,  #29            \n" // Load v0 with 8 copies of the 8 LSBs of L B factor =  0.114. 29/256 =  0.113 (29 = 0x001d).
                      "    movi        v1.8b,  #150           \n" // Load v1 with 8 copies of the 8 LSBs of L G factor =  0.587. 150/256 =  0.586 (150 = 0x0096).
                      "    movi        v2.8b,  #77            \n" // Load v2 with 8 copies of the 8 LSBs of L R factor =  0.299. 77/256 =  0.301 (77 = 0x004d).
-                     "0:						             \n"
+                     "0:                                                             \n"
                      // Read 8 BGRA pixels.
                      "    ld4         {v3.8b - v6.8b}, [%1],#32 \n" // Load 8 RGBA pixels, de-interleaving R into lower half of v3, G into lower half of v4, B into lower half of v5, A into lower half of v6.
                      // Do the weight average.
@@ -454,7 +454,7 @@ static void arVideoLumaABGRtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "    movi        v0.8b,  #29            \n" // Load v0 with 8 copies of the 8 LSBs of L B factor =  0.114. 29/256 =  0.113 (29 = 0x001d).
                      "    movi        v1.8b,  #150           \n" // Load v1 with 8 copies of the 8 LSBs of L G factor =  0.587. 150/256 =  0.586 (150 = 0x0096).
                      "    movi        v2.8b,  #77            \n" // Load v2 with 8 copies of the 8 LSBs of L R factor =  0.299. 77/256 =  0.301 (77 = 0x004d).
-                     "0:						             \n"
+                     "0:                                                             \n"
                      // Read 8 BGRA pixels.
                      "    ld4         {v3.8b - v6.8b}, [%1],#32 \n" // Load 8 ABGR pixels, de-interleaving A into lower half of v3, B into lower half of v4, G into lower half of v5, R into lower half of v6.
                      // Do the weight average.
@@ -479,7 +479,7 @@ static void arVideoLumaARGBtoL_ARM_neon_asm(uint8_t * __restrict dest, uint8_t *
                      "    movi        v0.8b,  #29            \n" // Load v0 with 8 copies of the 8 LSBs of L B factor =  0.114. 29/256 =  0.113 (29 = 0x001d).
                      "    movi        v1.8b,  #150           \n" // Load v1 with 8 copies of the 8 LSBs of L G factor =  0.587. 150/256 =  0.586 (150 = 0x0096).
                      "    movi        v2.8b,  #77            \n" // Load v2 with 8 copies of the 8 LSBs of L R factor =  0.299. 77/256 =  0.301 (77 = 0x004d).
-                     "0:						             \n"
+                     "0:                                                             \n"
                      // Read 8 BGRA pixels.
                      "    ld4         {v3.8b - v6.8b}, [%1],#32 \n" // Load 8 ARGB pixels, de-interleaving A into lower half of v3, R into lower half of v4, G into lower half of v5, B into lower half of v6.
                      // Do the weight average.
@@ -509,19 +509,19 @@ static void arVideoLumaBGRAtoL_Intel_simd_asm(uint8_t * __restrict dest, uint8_t
     do {
         __m128i pixels0_3 = _mm_load_si128(pin++); // pixels0_3 = [A3][R3][G3][B3][A2][R2][G2][B2][A1][R1][G1][B1][A0][R0][G0][B0].
         __m128i pixels4_7 = _mm_load_si128(pin++); // pixels4_7 = [A7][R7][G7][B7][A6][R6][G6][B6][A5][R5][G5][B5][A4][R4][G4][B4].
-        
+
         __m128i pixels0_3_l = _mm_unpacklo_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_l = 00[A1]00[R1]00[G1]00[B1]00[A0]00[R0]00[G0]00[B0].
         __m128i pixels0_3_h = _mm_unpackhi_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_h = 00[A3]00[R3]00[G3]00[B3]00[A2]00[R2]00[G2]00[B2].
         __m128i pixels4_7_l = _mm_unpacklo_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_l = 00[A5]00[R5]00[G5]00[B5]00[A4]00[R4]00[G4]00[B4].
         __m128i pixels4_7_h = _mm_unpackhi_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_h = 00[A7]00[R7]00[G7]00[B7]00[A6]00[R6]00[G6]00[B6].
-        
+
         __m128i y0_3_l = _mm_madd_epi16(pixels0_3_l, RGBScale);
         __m128i y0_3_h = _mm_madd_epi16(pixels0_3_h, RGBScale);
         __m128i y4_7_l = _mm_madd_epi16(pixels4_7_l, RGBScale);
         __m128i y4_7_h = _mm_madd_epi16(pixels4_7_h, RGBScale);
         __m128i y0_3 = _mm_hadd_epi32(y0_3_l, y0_3_h);
         __m128i y4_7 = _mm_hadd_epi32(y4_7_l, y4_7_h);
-        
+
         y0_3 = _mm_srli_epi32(y0_3, 8);
         y4_7 = _mm_srli_epi32(y4_7, 8);
         y0_3 = _mm_packs_epi32(y0_3, y0_3);
@@ -531,7 +531,7 @@ static void arVideoLumaBGRAtoL_Intel_simd_asm(uint8_t * __restrict dest, uint8_t
 
         *pout++ = _mm_cvtsi128_si32(y0_3);
         *pout++ = _mm_cvtsi128_si32(y4_7);
-        
+
         numPixelsDiv8--;
     } while (numPixelsDiv8);
 }
@@ -542,33 +542,33 @@ static void arVideoLumaRGBAtoL_Intel_simd_asm(uint8_t * __restrict dest, uint8_t
     uint32_t *pout = (uint32_t *)dest;
     int numPixelsDiv8 = numPixels >> 3;
     __m128i RGBScale = _mm_set_epi16(0, B8_CCIR601, G8_CCIR601, R8_CCIR601, 0, B8_CCIR601, G8_CCIR601, R8_CCIR601); // RGBScale = 000000[B8_CCIR601]00[G8_CCIR601]00[R8_CCIR601]000000[B8_CCIR601]00[G8_CCIR601]00[R8_CCIR601].
-    
+
     do {
         __m128i pixels0_3 = _mm_load_si128(pin++); // pixels0_3 = [A3][B3][G3][R3][A2][B2][G2][R2][A1][B1][G1][R1][A0][B0][G0][R0].
         __m128i pixels4_7 = _mm_load_si128(pin++); // pixels4_7 = [A7][B7][G7][R7][A6][B6][G6][R6][A5][B5][G5][R5][A4][B4][G4][R4].
-        
+
         __m128i pixels0_3_l = _mm_unpacklo_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_l = 00[A1]00[B1]00[G1]00[R1]00[A0]00[B0]00[G0]00[R0].
         __m128i pixels0_3_h = _mm_unpackhi_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_h = 00[A3]00[B3]00[G3]00[R3]00[A2]00[B2]00[G2]00[R2].
         __m128i pixels4_7_l = _mm_unpacklo_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_l = 00[A5]00[B5]00[G5]00[R5]00[A4]00[B4]00[G4]00[R4].
         __m128i pixels4_7_h = _mm_unpackhi_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_h = 00[A7]00[B7]00[G7]00[R7]00[A6]00[B6]00[G6]00[R6].
-        
+
         __m128i y0_3_l = _mm_madd_epi16(pixels0_3_l, RGBScale);
         __m128i y0_3_h = _mm_madd_epi16(pixels0_3_h, RGBScale);
         __m128i y4_7_l = _mm_madd_epi16(pixels4_7_l, RGBScale);
         __m128i y4_7_h = _mm_madd_epi16(pixels4_7_h, RGBScale);
         __m128i y0_3 = _mm_hadd_epi32(y0_3_l, y0_3_h);
         __m128i y4_7 = _mm_hadd_epi32(y4_7_l, y4_7_h);
-        
+
         y0_3 = _mm_srli_epi32(y0_3, 8);
         y4_7 = _mm_srli_epi32(y4_7, 8);
         y0_3 = _mm_packs_epi32(y0_3, y0_3);
         y4_7 = _mm_packs_epi32(y4_7, y4_7);
         y0_3 = _mm_packus_epi16(y0_3, y0_3);
         y4_7 = _mm_packus_epi16(y4_7, y4_7);
-        
+
         *pout++ = _mm_cvtsi128_si32(y0_3);
         *pout++ = _mm_cvtsi128_si32(y4_7);
-        
+
         numPixelsDiv8--;
     } while (numPixelsDiv8);
 }
@@ -579,33 +579,33 @@ static void arVideoLumaABGRtoL_Intel_simd_asm(uint8_t * __restrict dest, uint8_t
     uint32_t *pout = (uint32_t *)dest;
     int numPixelsDiv8 = numPixels >> 3;
     __m128i RGBScale = _mm_set_epi16(R8_CCIR601, G8_CCIR601, B8_CCIR601, 0, R8_CCIR601, G8_CCIR601, B8_CCIR601, 0); // RGBScale = 00[R8_CCIR601]00[G8_CCIR601]00[B8_CCIR601]000000[R8_CCIR601]00[G8_CCIR601]00[B8_CCIR601]0000.
-    
+
     do {
         __m128i pixels0_3 = _mm_load_si128(pin++); // pixels0_3 = [R3][G3][B3][A3][R2][G2][B2][A2][R1][G1][B1][A1][R0][G0][B0][A0].
         __m128i pixels4_7 = _mm_load_si128(pin++); // pixels4_7 = [R7][G7][B7][A7][R6][G6][B6][A6][R5][G5][B5][A5][R4][G4][B4][A4].
-        
+
         __m128i pixels0_3_l = _mm_unpacklo_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_l = 00[R1]00[G1]00[B1]00[A1]00[R0]00[G0]00[B0]00[A0].
         __m128i pixels0_3_h = _mm_unpackhi_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_h = 00[R3]00[G3]00[B3]00[A3]00[R2]00[G2]00[B2]00[A2].
         __m128i pixels4_7_l = _mm_unpacklo_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_l = 00[R5]00[G5]00[B5]00[A5]00[R4]00[G4]00[B4]00[A4].
         __m128i pixels4_7_h = _mm_unpackhi_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_h = 00[R7]00[G7]00[B7]00[A7]00[R6]00[G6]00[B6]00[A6].
-        
+
         __m128i y0_3_l = _mm_madd_epi16(pixels0_3_l, RGBScale);
         __m128i y0_3_h = _mm_madd_epi16(pixels0_3_h, RGBScale);
         __m128i y4_7_l = _mm_madd_epi16(pixels4_7_l, RGBScale);
         __m128i y4_7_h = _mm_madd_epi16(pixels4_7_h, RGBScale);
         __m128i y0_3 = _mm_hadd_epi32(y0_3_l, y0_3_h);
         __m128i y4_7 = _mm_hadd_epi32(y4_7_l, y4_7_h);
-        
+
         y0_3 = _mm_srli_epi32(y0_3, 8);
         y4_7 = _mm_srli_epi32(y4_7, 8);
         y0_3 = _mm_packs_epi32(y0_3, y0_3);
         y4_7 = _mm_packs_epi32(y4_7, y4_7);
         y0_3 = _mm_packus_epi16(y0_3, y0_3);
         y4_7 = _mm_packus_epi16(y4_7, y4_7);
-        
+
         *pout++ = _mm_cvtsi128_si32(y0_3);
         *pout++ = _mm_cvtsi128_si32(y4_7);
-        
+
         numPixelsDiv8--;
     } while (numPixelsDiv8);
 }
@@ -616,33 +616,33 @@ static void arVideoLumaARGBtoL_Intel_simd_asm(uint8_t * __restrict dest, uint8_t
     uint32_t *pout = (uint32_t *)dest;
     int numPixelsDiv8 = numPixels >> 3;
     __m128i RGBScale = _mm_set_epi16(B8_CCIR601, G8_CCIR601, R8_CCIR601, 0, B8_CCIR601, G8_CCIR601, R8_CCIR601, 0); // RGBScale = 00[B8_CCIR601]00[G8_CCIR601]00[R8_CCIR601]000000[B8_CCIR601]00[G8_CCIR601]00[R8_CCIR601]0000.
-    
+
     do {
         __m128i pixels0_3 = _mm_load_si128(pin++); // pixels0_3 = [B3][G3][R3][A3][B2][G2][R2][A2][B1][G1][R1][A1][B0][G0][R0][A0].
         __m128i pixels4_7 = _mm_load_si128(pin++); // pixels4_7 = [B7][G7][R7][A7][B6][G6][R6][A6][B5][G5][R5][A5][B4][G4][R4][A4].
-        
+
         __m128i pixels0_3_l = _mm_unpacklo_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_l = 00[B1]00[G1]00[R1]00[A1]00[B0]00[G0]00[R0]00[A0].
         __m128i pixels0_3_h = _mm_unpackhi_epi8(pixels0_3, _mm_setzero_si128()); // pixels0_3_h = 00[B3]00[G3]00[R3]00[A3]00[B2]00[G2]00[R2]00[A2].
         __m128i pixels4_7_l = _mm_unpacklo_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_l = 00[B5]00[G5]00[R5]00[A5]00[B4]00[G4]00[R4]00[A4].
         __m128i pixels4_7_h = _mm_unpackhi_epi8(pixels4_7, _mm_setzero_si128()); // pixels4_7_h = 00[B7]00[G7]00[R7]00[A7]00[B6]00[G6]00[R6]00[A6].
-        
+
         __m128i y0_3_l = _mm_madd_epi16(pixels0_3_l, RGBScale);
         __m128i y0_3_h = _mm_madd_epi16(pixels0_3_h, RGBScale);
         __m128i y4_7_l = _mm_madd_epi16(pixels4_7_l, RGBScale);
         __m128i y4_7_h = _mm_madd_epi16(pixels4_7_h, RGBScale);
         __m128i y0_3 = _mm_hadd_epi32(y0_3_l, y0_3_h);
         __m128i y4_7 = _mm_hadd_epi32(y4_7_l, y4_7_h);
-        
+
         y0_3 = _mm_srli_epi32(y0_3, 8);
         y4_7 = _mm_srli_epi32(y4_7, 8);
         y0_3 = _mm_packs_epi32(y0_3, y0_3);
         y4_7 = _mm_packs_epi32(y4_7, y4_7);
         y0_3 = _mm_packus_epi16(y0_3, y0_3);
         y4_7 = _mm_packus_epi16(y4_7, y4_7);
-        
+
         *pout++ = _mm_cvtsi128_si32(y0_3);
         *pout++ = _mm_cvtsi128_si32(y4_7);
-        
+
         numPixelsDiv8--;
     } while (numPixelsDiv8);
 }
